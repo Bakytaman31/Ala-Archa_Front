df98c432dc9d85ffd6e47974cba25b71c78c803f
aisalkyn <aisalkyn.baizakova@gmail.com>
2022-03-27 23:08:38 +0600

    add 3d slider

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Carousel } from "3d-react-carousal";
+import carousel1 from "../../assets/slider_IMG1.png";
+import carousel2 from "../../assets/slider_IMG2.png";
+import carousel3 from "../../assets/slider_IMG3.png";
+import carousel4 from "../../assets/slider_IMG4.png";
+import "./Carousel.css";
+
+const Caroгsel = () => {
+  let slides = [
+    <img src={carousel1} alt="1" />,
+    <img src={carousel2} alt="2" />,
+    <img src={carousel3} alt="3" />,
+    <img src={carousel4} alt="4" />,
+    <img src={carousel2} alt="5" />,
+  ];
+
+  const callback = function (index) {
+    console.log("callback", index);
+  };
+
+  return (
+    <>
+      <Carousel
+        slides={slides}
+        autoplay={true}
+        interval={1500}
+        onSlideChange={callback}
+      />
+    </>
+  );
+};
+
+export default Caroгsel;
